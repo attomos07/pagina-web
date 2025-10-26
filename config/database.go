a06e31b686fdc75898901361518b0ff1b5c9ce89
scott-hurtado <hurtadoscott3@gmail.com>
2025-10-26 03:05:24 -0700

    Actualicé la lógica de X

--- config/database.go
+++ config/database.go
@@ -12,17 +12,26 @@ import (
 
 var DB *gorm.DB
 
-// ConnectDatabase establece la conexión con MySQL
 func ConnectDatabase() {
-	host := os.Getenv("MYSQL_HOST")
-	port := os.Getenv("MYSQL_PORT")
-	database := os.Getenv("MYSQL_DATABASE")
-	user := os.Getenv("MYSQL_USER")
-	password := os.Getenv("MYSQL_PASSWORD")
+	// Primero intentar con MYSQL_PUBLIC_URL (Railway)
+	publicURL := os.Getenv("MYSQL_PUBLIC_URL")
 
-	// Construir DSN (Data Source Name)
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		user, password, host, port, database)
+	var dsn string
+
+	if publicURL != "" {
+		// Usar la URL pública directamente
+		dsn = publicURL
+	} else {
+		// Construir DSN manualmente (para desarrollo local)
+		host := os.Getenv("MYSQL_HOST")
+		port := os.Getenv("MYSQL_PORT")
+		database := os.Getenv("MYSQL_DATABASE")
+		user := os.Getenv("MYSQL_USER")
+		password := os.Getenv("MYSQL_PASSWORD")
+
+		dsn = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+			user, password, host, port, database)
+	}
 
 	// Conectar a la base de datos
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
@@ -37,7 +46,6 @@ func ConnectDatabase() {
 	log.Println("✅ Conexión exitosa con MySQL")
 }
 
-// GetDB retorna la instancia de la base de datos
 func GetDB() *gorm.DB {
 	return DB
 }
